59665b627ce08103a9907ebadd5ed744a1e66313
Glycan Zyzzy <wisepoison@gmail.com>
2011-09-03 11:38:45 -0400

    Ball objects and several balls

--- bounce.py
+++ bounce.py
@@ -1,37 +1,55 @@
 import pygame, sys
 
 pygame.init()
+c = pygame.time.Clock()
 
+white = 255, 255, 255
 size = h, w = [600, 600]
+
 speed = [5, 7]
 fric = 0.9
 
-white = 255, 255, 255
-
 blank = pygame.Surface(size)
 blank.fill(white)
-blank.set_alpha(64)
+blank.set_alpha(32)
 
 screen = pygame.display.set_mode(size)
 
-c = pygame.time.Clock()
+class Ball(pygame.sprite.Sprite):
+    def __init__(self,  name, x = 300, y = 300, xspeed = 5, yspeed = 6):
+        self.rect = ballpic.get_rect()
+        self.rect.move_ip((x, y))
+        self.speed =[xspeed, yspeed]
+        self.name = name
 
-ball = pygame.image.load("ball.png").convert_alpha()
-ballr = ball.get_rect()
+    def boarders(self):
+        if self.rect.top < 0 or self.rect.bottom > h:
+            self.speed[1] *= -1
+        if self.rect.left < 0 or self.rect.right > w:
+            self.speed[0] *= -1
+
+    def move(self):
+        self.rect.move_ip(self.speed)
+        print self, "moved"
+
+    def __str__(self):
+        return "<" + self.name + ">"
+
+ballpic = pygame.image.load("ball.png").convert_alpha()
+
+balls = [Ball('ball1', 200, 200, -7, -8), Ball("ball2")]
 
 screen.fill(white)
 
-print "running, rect:", ballr
 
 while 1:
-    ballr = ballr.move(speed)
-    if ballr.top < 0 or ballr.bottom > h:
-        speed[1] *= -1
-    if ballr.left < 0 or ballr.right > w:
-        speed[0] *= -1
+    for ball in balls:
+        print "Working on", ball
+        ball.move()
+        ball.boarders()
+        screen.blit(ballpic, ball.rect)
 
     screen.blit(blank, blank.get_rect())
-    screen.blit(ball, ballr)
     pygame.display.flip()
     c.tick(70)
     #print "FPS:",c.get_fps()
