a830fd45e6abad302e9274b08cb721c8912bc017
Glycan Zyzzy <wisepoison@gmail.com>
2011-09-03 10:55:06 -0400

    Trail

--- bounce.py
+++ bounce.py
@@ -3,10 +3,14 @@ import pygame, sys
 pygame.init()
 
 size = h, w = [600, 600]
-speed = [5, 4]
+speed = [5, 7]
 fric = 0.9
 
 white = 255, 255, 255
+
+blank = pygame.Surface(size)
+blank.fill(white)
+blank.set_alpha(64)
 
 screen = pygame.display.set_mode(size)
 
@@ -14,15 +18,19 @@ c = pygame.time.Clock()
 
 ball = pygame.image.load("ball.png").convert_alpha()
 ballr = ball.get_rect()
-print "running, rect:", ballr
+
 screen.fill(white)
+
+print "running, rect:", ballr
+
 while 1:
     ballr = ballr.move(speed)
     if ballr.top < 0 or ballr.bottom > h:
         speed[1] *= -1
     if ballr.left < 0 or ballr.right > w:
         speed[0] *= -1
-    #screen.fill(white)
+
+    screen.blit(blank, blank.get_rect())
     screen.blit(ball, ballr)
     pygame.display.flip()
     c.tick(70)
