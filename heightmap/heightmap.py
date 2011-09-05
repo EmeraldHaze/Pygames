de831d5cc53153ac47d159b5346c2719da847d86
Glycan Zyzzy <wisepoison@gmail.com>
2011-09-04 22:47:10 -0400

    H ightmaps

--- heightmap/heightmap.py
+++ heightmap/heightmap.py
@@ -2,16 +2,16 @@ import pygame
 from numpy import random, zeros
 
 size = w, h = [600, 600]
-grid = random.randint(256, (w, h, 3))
-newgrid = numpy.zeros((w, h, 3), int)
+grid = random.randint(256, size = (w, h, 3))
+newgrid = zeros((w, h, 3), int)
 
-#pygame.init()
-#screen = pygame.display.set_mode(size)
-#pygame.display.set_caption("heightmap")
-#screen.fill()
-    
-sarr = pygame.surfarray.pixels2d(screen)
+for y in range(1, w):
+    for x in range(1, h):
+        colors = grid[y-1:y+2, x-1:x+2]
+        newcolor = [sum(color)/len(color) for color in zip(colors)]
+        newgrid[y, x, :] = newcolor
 
-for i in range(1, w):
-    for h in range(1, h):
-        print sarr[w,h]
+pygame.init()
+
+image = pygame.surfarray.make_surface(newgrid)
+pygame.image.save(image, "image.png")
