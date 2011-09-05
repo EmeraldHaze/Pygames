f7af65abe4c9960da1ba6640990731b742a35017
Glycan Zyzzy <wisepoison@gmail.com>
2011-09-04 23:24:44 -0400

    Should work

--- heightmap/heightmap.py
+++ heightmap/heightmap.py
@@ -7,9 +7,13 @@ newgrid = zeros((w, h, 3), int)
 
 for y in range(1, w):
     for x in range(1, h):
-        colors = grid[y-1:y+2, x-1:x+2]
-        newcolor = [sum(color)/len(color) for color in zip(colors)]
-        newgrid[y, x, :] = newcolor
+        colors = []
+        for row in grid[y-1:y+2, x-1:x+2]: colors.extend(row)
+        for i in range(len(colors)): colors[i] = list(colors[i])
+        newcolor = [sum(color)/len(color) for color in zip(*colors)]
+        for c in range(len(newgrid[y, x])):
+            newgrid[y, x, c] = newcolor[c]
+
 
 pygame.init()
 
