ab9c3bfa3ab06f1df33d4935c85fee216fc15e72
Yelik Newo <yeliknewo@gmail.com>
2011-09-05 01:10:39 -0400

    stuff with images

--- heightmap/yel/heightmap.py
+++ heightmap/yel/heightmap.py
@@ -1,12 +1,10 @@
 import pygame
 from numpy import random, zeros
+from random import randint
 
-size = w, h = [100, 100]
-grid = random.randint(256, size = (w, h))
+size = w, h = [300, 300]
+grid = zeros((w, h), int)
 newgrid = zeros((w, h), int)
-
-print grid
-print newgrid
 
 def getat(x,y):
     n = 0
@@ -14,17 +12,36 @@ def getat(x,y):
         n = grid[x,y]
     return n
 
-for i in range(input('Smooth how much? ')):
+for x in range(0, w):
+    for y in range(0, h):
+        grid[x,y] = (x - y) - (y - x)
+
+for i in range(0):
     for x in range(0, w):
         for y in range(0, h):
             newgrid[x,y] = (getat(x,y) + getat(x-1,y) + getat(x,y+1) + getat(x-1,y) + getat(x,y-1)) / 5
     grid = newgrid
+    print i+1
 
 colorgrid = zeros((w, h, 3), int)
 for x in range(0,w):
     for y in range(0,h):
-        colorgrid[x,y,1] = grid[x,y]
+        for i in range(1):
+            colorgrid[x,y,1] = grid[x,y]
 
 pygame.init()
 image = pygame.surfarray.make_surface(colorgrid)
 pygame.image.save(image, "image.png")
+
+print 'Done'
+
+#1, x * y
+#2, (x * 3) * (y * 3)
+#3, (x ** 2) + (y ** 2)
+#4, (x ** 2) * 5 + (y ** 2) * 5
+#5, (x ** 2) * (y ** 2)
+#6, (x ** 2) * 5 * (y ** 2) * 5
+#7, x - y
+#8, y - x
+#9, x - y * y - x
+#10, (x - y) - (y - x)
