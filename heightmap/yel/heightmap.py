4ed4e2f69284ef55b8bdd23e4bcfc061a32b19e2
Yelik Newo <yeliknewo@gmail.com>
2011-09-04 23:47:22 -0400

    added mine

---
+++ heightmap/yel/heightmap.py
@@ -0,0 +1,30 @@
+import pygame
+from numpy import random, zeros
+
+size = w, h = [100, 100]
+grid = random.randint(256, size = (w, h))
+newgrid = zeros((w, h), int)
+
+print grid
+print newgrid
+
+def getat(x,y):
+    n = 0
+    if x >= 0 and x < w and y >= 0 and y < h:
+        n = grid[x,y]
+    return n
+
+for i in range(10):
+    for x in range(0, w):
+        for y in range(0, h):
+            newgrid[x,y] = (getat(x,y) + getat(x-1,y) + getat(x,y+1) + getat(x-1,y) + getat(x,y-1)) / 5
+    print newgrid
+    grid = newgrid
+
+pygame.init()
+colorgrid = zeros((w, h, 3), int)
+for x in range(0,w):
+    for y in range(0,h):
+        colorgrid[x,y,1] = grid[x,y]
+image = pygame.surfarray.make_surface(colorgrid)
+pygame.image.save(image, "image.png")
