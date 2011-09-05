d8b653c3e428965312a166bf3ef088f7c7bc2439
Yelik Newo <yeliknewo@gmail.com>
2011-09-04 22:37:27 -0400

    fixed name

--- grid/grid.py
+++ grid/grid.py
@@ -1,9 +1,22 @@
-import pygame
-
-pygame.init()
+import pygame, sys
 
 size = h, w = [600, 600]
+tpf = 10
 
-surface = pygame.Surface(size)
+white = (255,255,255)
 
+pygame.init()
+pygame.display.set_caption('Grid')
 screen = pygame.display.set_mode(size)
+background = pygame.Surface(size)
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print 'Closing'
+            sys.exit()
+
+    for i in range(tpf):
+        background.fill(white)
+
+    pygame.display.flip()
--- heightmap/heightmap.py
+++ heightmap/heightmap.py
@@ -5,5 +5,13 @@ size = w, h = [600, 600]
 grid = random.randint(256, (w, h, 3))
 newgrid = numpy.zeros((w, h, 3), int)
 
+#pygame.init()
+#screen = pygame.display.set_mode(size)
+#pygame.display.set_caption("heightmap")
+#screen.fill()
+    
+sarr = pygame.surfarray.pixels2d(screen)
+
 for i in range(1, w):
     for h in range(1, h):
+        print sarr[w,h]
